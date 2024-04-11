e859a1621c87c0e42d28b7705ed808b0eec32011
indramhaarta <indramhrt@gmail.com>
2024-04-11 16:34:16 +0800

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by I Made Indra Mahaarta on 11/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
